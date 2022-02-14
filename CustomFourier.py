aba6b31c9dc5a4bb185a57e36d284c9098506473
Robert Boerwinkle <48035569+rfboerwinkle@users.noreply.github.com>
2022-02-14 16:05:50 -0600

    Fourier.CheckBounds function
    sets coords outside the bounds of the original data to a little more than the max distance from the camera

--- CustomFourier.py
+++ CustomFourier.py
@@ -6,7 +6,8 @@ import time
 # ~ this is a class to have pre-computed values
 class Fourier:
 	def __init__(self, data):
-		# ~ TODO maybe test a couple and find the fastest
+		# ~ TODO maybe test a couple and find the fastest, also see what data can be thrown, if any
+		self.bounds = data.shape
 		self.trans = np.fft.fftn(data) 
 		self.u,self.v,self.w = np.mgrid[:self.trans.shape[0], :self.trans.shape[1], :self.trans.shape[2]] * 1j * 2 * np.pi
 		self.u /= self.trans.shape[0]
@@ -17,15 +18,12 @@ class Fourier:
 		self.w = np.reshape(self.w, (self.w.size))
 		self.trans = np.reshape(self.trans, (self.trans.size))
 
-
-
 	def eval(self, coords):
 		return np.sum((self.trans * np.exp(self.u*coords[0] + self.v*coords[1] + self.w*coords[2])).real) / self.trans.size
 		# ~ this works only in 3D.
 		# ~ coords: coordinates of value you want to find, in the order of numpy's axes.  It should be a 1x3 array
 		# ~ returns: w, the value at the coordinates
 		# ~ adapted from https://www.originlab.com/doc/Origin-Help/InverseFFT2-Algorithm
-		# ~ TODO possibly this should be made to take a 1D array of points (which would make it a 2D array), but that might take up too much memory.  Currently to do multiple points, just call this function in a loop
 	
 	def ParallelEval(self, coords):
 		X = coords[:, 0]
@@ -36,4 +34,13 @@ class Fourier:
 		# ~ end = time.time()
 		# ~ print(end - start)
 		return w
-		
+
+	def CheckBounds(self, Vectors, coords, origin, MaxDistance):
+		X = coords[:, 0]
+		Y = coords[:, 1]
+		Z = coords[:, 2]
+		mask = X>0 * X<self.bounds[0] * Y>0 * Y<self.bounds[1] * Z>0 * Z<self.bound[2]
+		T[mask, 0] = origin[0] + MaxDistance * Vectors[:, 0] * 1.1
+		T[mask, 1] = origin[1] + MaxDistance * Vectors[:, 1] * 1.1
+		T[mask, 2] = origin[2] + MaxDistance * Vectors[:, 2] * 1.1
+		return T
